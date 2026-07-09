399a86f160076ed62c9af532613abe1f63e5d6ea
Danel-jg <danieljguerra11@gmail.com>
2026-07-08 21:47:07 -0500

    basic data types in cpp

---
+++ variables/variables.cpp
@@ -0,0 +1,28 @@
+//data types in cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 10; // integer data type
+    cout << "Integer value: " << a << endl;
+
+    float b = 3.14; // float data type
+    cout << "Float value: " << b << endl;
+
+    double c = 3.14159; // double data type
+    cout << "Double value: " << c << endl;
+
+    char d = 'D'; // character data type
+    cout << "Character value: " << d << endl;
+
+    bool e = true; // boolean data type, 1 = true, 0 = false
+    cout << "Boolean value 1 = true: " << e << endl;
+
+    bool f = false; // boolean data type, 1 = true, 0 = false
+    cout << "Boolean value 0 = false: " << f << endl;
+
+
+
+    return 0;
+}
